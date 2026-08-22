002aa968f3f6b090196932b65cdddbe533ec6a94
stodolkiewicz <stodoo@gmail.com>
2026-08-22 08:23:00 +0200

    enable configuration properties scanning

--- assistant-app/src/main/java/com/cdq/assistant/AssistantApplication.java
+++ assistant-app/src/main/java/com/cdq/assistant/AssistantApplication.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AssistantApplication {
 
 	public static void main(String[] args) {
